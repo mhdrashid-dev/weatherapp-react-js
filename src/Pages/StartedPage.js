d8d90559a22e9fec4a66c4e278954c76898d717e
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-06 22:19:15 +0530

    Creating starterpage

---
+++ src/Pages/StartedPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Starter from '../Components/StarterPage/Starter'
+
+function StartedPage() {
+  return (
+    <div>
+      <Starter></Starter>
+    </div>
+  )
+}
+
+export default StartedPage
