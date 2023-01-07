76bcb53f262367850d4137ea1b5b5ae9930ee380
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-07 12:32:38 +0530

    Install and complete router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import StartedPage from './Pages/StartedPage'
 import WeatherPage from './Pages/WeatherPage'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <StartedPage></StartedPage>
-      <WeatherPage></WeatherPage>      
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<StartedPage></StartedPage>}></Route>
+          <Route path='/weatherpage' element={<WeatherPage></WeatherPage>}></Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
